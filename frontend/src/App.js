be6e0cec5d2599a5bd3ddef64f193e0ca9e584b9
Jackson Chen <90215880+Sma1lboy@users.noreply.github.com>
2024-02-14 20:48:01 -0600

    to: adding tailwindcss support with react UI framework nextui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,13 @@
+import { Avatar, Button, NextUIProvider } from "@nextui-org/react";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <NextUIProvider>
+      <div>asdasds</div>
+      <Button color="primary">Button</Button>
+      <Avatar name="JC"></Avatar>
+    </NextUIProvider>
+  );
 }
 
 export default App;
