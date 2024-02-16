5f07be2eb8e12ec814e4ff861c404c5316268488
Jackson Chen <90215880+Sma1lboy@users.noreply.github.com>
2024-02-16 17:16:02 -0600

    to: adding linear background on all page base on figma

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,16 @@ import Main from "./Main";
 function App() {
   return (
     <NextUIProvider>
-      <Main />
+      {/* Header */}
+      <div
+        style={{
+          height: "100%",
+          background: "linear-gradient(rgb(246,237,189), rgba(246,237,189, 0))",
+        }}
+      >
+        <Main />
+      </div>
+      {/* footer */}
     </NextUIProvider>
   );
 }
