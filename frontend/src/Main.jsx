87a84e6e4ac07e215152daa84706d483ad8c9fdf
Jackson Chen <90215880+Sma1lboy@users.noreply.github.com>
2024-02-16 17:16:34 -0600

    feat: basically finish ui of main page, encapulation PostCards and PostCard comp to generate post msg

--- frontend/src/Main.jsx
+++ frontend/src/Main.jsx
@@ -6,41 +6,96 @@ import {
   Divider,
   Input,
 } from "@nextui-org/react";
-import { VscArchive, VscComment, VscHeart } from "react-icons/vsc";
+import {
+  VscArchive,
+  VscArrowSmallLeft,
+  VscArrowSmallRight,
+  VscComment,
+  VscHeart,
+} from "react-icons/vsc";
 import SearchIcon from "./components/icons/SearchIcon";
 
 const Main = () => {
   return (
     <div style={{ display: "flex", flexDirection: "column", padding: "50px" }}>
-      <div>
-        <h1 style={{ fontSize: 30 }}>What’s everyone up to?</h1>
-        <p style={{ color: "rgba(0,0,0,.7)" }}>
-          Leave a comment or give them a heart
-        </p>
-      </div>
-      <div style={{ width: "300px", margin: "20px 0px" }}>
-        <Input
-          placeholder="Type to search..."
-          size="sm"
-          radius="full"
-          startContent={<SearchIcon size={18} />}
-          type="search"
-        />
-      </div>
-
       <div
+        className="main-left"
         style={{
+          width: "60%",
+          height: "inherit",
           display: "inherit",
-          flexDirection: "column",
+          flexDirection: "inherit",
         }}
       >
-        <PostCard>Jackson Say hi</PostCard>
-        <PostCard>Jackson Say hi</PostCard>
+        {" "}
+        <div>
+          <h1 style={{ fontSize: 30 }}>What’s everyone up to?</h1>
+          <p style={{ color: "rgba(0,0,0,.7)" }}>
+            Leave a comment or give them a heart
+          </p>
+        </div>
+        <div style={{ width: "300px", margin: "20px 0px" }}>
+          <Input
+            placeholder="Type to search..."
+            size="sm"
+            radius="full"
+            startContent={<SearchIcon size={18} />}
+            type="search"
+          />
+        </div>
+        <PostCards>
+          <PostCard>Jackson Say hi</PostCard>
+          <PostCard>Jackson Say hi</PostCard>
+        </PostCards>
+        <div
+          style={{
+            display: "inherit",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <PagingCard></PagingCard>
+          <Button color="primary">Write A New Post</Button>
+        </div>
       </div>
     </div>
   );
 };
-
+let PagingCard = () => {
+  return (
+    <Card style={{ width: "350px", height: "65px" }}>
+      <CardBody
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-around ",
+        }}
+      >
+        <Button isIconOnly>
+          <VscArrowSmallLeft style={{ width: "20px", height: "20px" }} />
+        </Button>
+        <Button isIconOnly>1</Button>
+        <Button isIconOnly>2</Button>
+        <Button isIconOnly>3</Button>
+        <Button isIconOnly>
+          <VscArrowSmallRight style={{ width: "20px", height: "20px" }} />
+        </Button>
+      </CardBody>
+    </Card>
+  );
+};
+let PostCards = ({ children }) => {
+  return (
+    <div
+      style={{
+        display: "inherit",
+        flexDirection: "column",
+      }}
+    >
+      {children}
+    </div>
+  );
+};
 let PostCard = ({
   children,
   avatar = "JC",
@@ -53,7 +108,6 @@ let PostCard = ({
   return (
     <Card
       style={{
-        width: "60%",
         minWidth: "450px",
         height: "250px",
         marginBottom: "30px",
