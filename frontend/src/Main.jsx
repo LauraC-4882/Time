6e39acc4ea7b660cf2115c244e0be0f19f4b98e4
Jackson Chen <90215880+Sma1lboy@users.noreply.github.com>
2024-02-19 08:30:09 -0600

    feat: create basically post modal

--- frontend/src/Main.jsx
+++ frontend/src/Main.jsx
@@ -1,23 +1,10 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import {
-  Avatar,
-  Button,
-  Card,
-  CardBody,
-  Divider,
-  Input,
-} from "@nextui-org/react";
-import {
-  VscArchive,
-  VscArrowSmallLeft,
-  VscArrowSmallRight,
-  VscComment,
-  VscHeart,
-} from "react-icons/vsc";
-import SearchIcon from "./components/icons/SearchIcon";
-
-import React, { useState } from "react";
+import {Button, Card, CardBody, Divider, Input} from "@nextui-org/react";
+import React, {useState} from "react";
+import {VscArrowSmallLeft, VscArrowSmallRight} from "react-icons/vsc";
 import ModalCard from "./components/ModalCard";
+import {PostCard, PostCards} from "./components/PostCardsComp";
+import SearchIcon from "./components/icons/SearchIcon";
 const Main = () => {
   const [openModal, setOpenModal] = useState(false);
   let handleOpenPost = () => {
@@ -27,7 +14,7 @@ const Main = () => {
     setOpenModal(false);
   };
   return (
-    <div style={{ display: "flex", flexDirection: "column", padding: "50px" }}>
+    <div style={{display: "flex", flexDirection: "column", padding: "50px"}}>
       <div
         className="main-left"
         style={{
@@ -39,12 +26,10 @@ const Main = () => {
       >
         {" "}
         <div>
-          <h1 style={{ fontSize: 30 }}>What’s everyone up to?</h1>
-          <p style={{ color: "rgba(0,0,0,.7)" }}>
-            Leave a comment or give them a heart
-          </p>
+          <h1 style={{fontSize: 30}}>What’s everyone up to?</h1>
+          <p style={{color: "rgba(0,0,0,.7)"}}>Leave a comment or give them a heart</p>
         </div>
-        <div style={{ width: "300px", margin: "20px 0px" }}>
+        <div style={{width: "300px", margin: "20px 0px"}}>
           <Input
             placeholder="Type to search..."
             size="sm"
@@ -69,22 +54,32 @@ const Main = () => {
             Write A New Post
           </Button>
         </div>
-        <PostModal
-          handleQuit={handleRequestQuitPost}
-          isOpen={openModal}
-        ></PostModal>
+        <PostModal handleQuit={handleRequestQuitPost} isOpen={openModal}></PostModal>
       </div>
     </div>
   );
 };
 
-let PostModal = ({ handleQuit, isOpen }) => {
-  return <ModalCard handleQuit={handleQuit} isOpen={isOpen}></ModalCard>;
+let PostModal = ({handleQuit, isOpen}) => {
+  return (
+    <ModalCard handleQuit={handleQuit} isOpen={isOpen}>
+      <div style={{display: "flex", flexDirection: "column"}}>
+        <p style={{fontSize: 20, fontWeight: 700}}>Write your Own Post</p>
+        <Divider />
+        <p style={{fontSize: 14, fontWeight: 700}}>Do you want to tell others?</p>
+        <div style={{display: "flex", width: "25%", justifyContent: "space-between"}}>
+          <Button color="primary">Yes</Button>
+          <Button color="primary">No</Button>
+        </div>
+        <p style={{fontSize: 14, fontWeight: 700}}>Wite in your own words</p>
+      </div>
+    </ModalCard>
+  );
 };
 
 let PagingCard = () => {
   return (
-    <Card style={{ width: "350px", height: "65px" }}>
+    <Card style={{width: "350px", height: "65px"}}>
       <CardBody
         style={{
           display: "flex",
@@ -93,104 +88,14 @@ let PagingCard = () => {
         }}
       >
         <Button isIconOnly>
-          <VscArrowSmallLeft style={{ width: "20px", height: "20px" }} />
+          <VscArrowSmallLeft style={{width: "20px", height: "20px"}} />
         </Button>
         <Button isIconOnly>1</Button>
         <Button isIconOnly>2</Button>
         <Button isIconOnly>3</Button>
         <Button isIconOnly>
-          <VscArrowSmallRight style={{ width: "20px", height: "20px" }} />
+          <VscArrowSmallRight style={{width: "20px", height: "20px"}} />
         </Button>
-      </CardBody>
-    </Card>
-  );
-};
-let PostCards = ({ children }) => {
-  return (
-    <div
-      style={{
-        display: "inherit",
-        flexDirection: "column",
-      }}
-    >
-      {children}
-    </div>
-  );
-};
-let PostCard = ({
-  children,
-  avatar = "JC",
-  name = "Jackson Chen",
-  userId = null,
-}) => {
-  let userLike = userId ? userId : 123;
-  let userComments = userId ? userId : 321;
-  let userArchive = userId ? userId : 213;
-  return (
-    <Card
-      style={{
-        minWidth: "450px",
-        height: "250px",
-        marginBottom: "30px",
-      }}
-    >
-      <CardBody>
-        <div
-          style={{
-            height: "30%",
-            display: "flex",
-            alignItems: "center",
-            marginTop: "2%",
-          }}
-        >
-          <Avatar
-            size="sm"
-            name={name}
-            color="primary"
-            src={avatar === "JC" ? "" : avatar}
-          />
-          <span style={{ marginLeft: "10px" }}>{name}</span>
-        </div>
-        <Divider
-          style={{ margin: "2% 0", width: "75%", alignSelf: "center " }}
-        />
-        <div style={{ height: "40%" }}>
-          <span>{children}</span>
-        </div>
-
-        <Divider style={{ margin: "2% 0" }} />
-        <div style={{ display: "flex", justifyContent: "space-between" }}>
-          <div
-            style={{
-              display: "inherit",
-              width: "40%",
-              justifyContent: "space-between",
-            }}
-          >
-            <div style={{ display: "inherit", alignItems: "center" }}>
-              <VscHeart></VscHeart>
-              <span style={{ marginLeft: "2px", fontSize: 14 }}>
-                {userLike}
-              </span>
-            </div>
-            <div style={{ display: "inherit", alignItems: "center" }}>
-              <VscComment></VscComment>
-              <span style={{ marginLeft: "2px", fontSize: 14 }}>
-                {userComments}
-              </span>
-            </div>
-            <div style={{ display: "inherit", alignItems: "center" }}>
-              <VscArchive></VscArchive>
-              <span style={{ marginLeft: "2px", fontSize: 14 }}>
-                {userArchive}
-              </span>
-            </div>
-          </div>
-
-          <Button color="primary" style={{ marginRight: "4%" }}>
-            Reply
-          </Button>
-        </div>
       </CardBody>
     </Card>
   );
