d9498c4922bb76a768194f9826687377c1eeba7e
bobiboba <dc4886@nyu.edu>
2024-04-07 19:09:35 -0400

    Commit again with just about page

---
+++ frontend/src/pages/about/ContentOffer.js
@@ -0,0 +1,21 @@
+// about/ContentOffer.js
+// contents array
+export let content =()=> [
+  {img:"https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg",
+  head:"Wanna save a time capsule for your future?",
+  p1:"- We will send this private letter to you at your targeted time at targeted location",
+  p2: "- Open the time capsule one day in the future to revoke the current unforgettable-memories"
+},
+{img:"https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg",
+head:"Wanna save a time capsule for your future?",
+p1:"- We will send this private letter to you at your targeted time at targeted location",
+p2: "- Open the time capsule one day in the future to revoke the current unforgettable-memories"
+},
+{img:"https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg",
+  head:"Wanna save a time capsule for your future?",
+  p1:"- We will send this private letter to your receivers at your wished time in any form you want!",
+  p2: "-"
+}
+
+
+]
---
+++ frontend/src/pages/about/Information.jsx
@@ -0,0 +1,39 @@
+import {Image,Card, CardHeader, CardBody} from "@nextui-org/react";
+import {Spacer} from "@nextui-org/react";
+import {CustomCard3} from "./CustomCard3";
+import {CustomCard1} from "./CustomCard1";
+import {CustomCard2} from "./CustomCard2";
+import {CustomCard4} from "./CustomCard4";
+
+
+
+let Information = () =>{
+    return(
+        <><div class="Aboutbutton" style={{ display: "flex", flexDirection: "column" }}>
+          <center>
+        <div style={{display:"flex",color:"#D97706", fontSize:"10px", fontWeight:"bold",justifyContent:"center"}}>
+        <h >Our Team</h>
+        </div>
+          <div style={{display:"flex",color:"black", fontSize:"30px", fontWeight:"bold",justifyContent:"center"}}>
+        <h >Our Awesome Team
+</h>
+        </div>
+        <div style={{display:"flex",color:"gray",fontSize:"20px",justifyContent:"center"}}>
+        <h>Why are we doing this
+</h></div>
+        <div style={{display:"flex",color:"gray",fontSize:"15px",justifyContent:"center"}}>
+        <p>We want to be the small drops to form the warm ocean
+
+</p></div>
+        </center>
+      </div><div class="cards" >
+      <div className="flex" style={{flexDirection:"row", justifyContent:"center", margin:"30px"}}>
+        
+      <CustomCard1 />
+    </div>
+
+        </div></>
+
+    )
+}
+export default Information;
