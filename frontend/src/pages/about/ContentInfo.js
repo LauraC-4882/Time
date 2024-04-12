d9c551681f1cdd8b10eaea06cd9d1c0171de6a56
Jackson Chen <90215880+Sma1lboy@users.noreply.github.com>
2024-04-12 17:18:51 -0500

    Refactor ContentInfo and InfoCard components in about page, update data structure and props

--- frontend/src/pages/about/ContentInfo.js
+++ frontend/src/pages/about/ContentInfo.js
@@ -1,22 +1,25 @@
 // about/ContentInfo.js
 // contents array
-export let ContentInfo =()=> [
-    {img:"https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg",
-    role:"Designers",
-    description:"Roles+contact info"
+export const ContentInfo = [
+  {
+    img: "https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg",
+    role: "Designers",
+    description: "Roles+contact info",
   },
-  {img:"https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg",
-    role:"Frontend",
-    description:"Roles+contact info"
+  {
+    img: "https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg",
+    role: "Frontend",
+    description: "Roles+contact info",
   },
-  {img:"https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg",
-    role:"Backend",
-    description:"Roles+contact info"
+  {
+    img: "https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg",
+    role: "Backend",
+    description: "Roles+contact info",
   },
-  {img:"https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg",
-    role:"Publishing",
-    description:"Roles+contact info"
-  }
-  
-  
-  ]
+  {
+    img: "https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg",
+    role: "Publishing",
+    description: "Roles+contact info",
+  },
+];
+export default ContentInfo;
