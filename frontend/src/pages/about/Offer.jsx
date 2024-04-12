8aa8a2548eb33fd141be8af5c8988aa630ad7bf5
Jackson Chen <90215880+Sma1lboy@users.noreply.github.com>
2024-04-12 17:18:45 -0500

    Refactor ContentOffer and Offer components in about page, make fake data as array, passing param to Card

--- frontend/src/pages/about/Offer.jsx
+++ frontend/src/pages/about/Offer.jsx
@@ -1,30 +1,54 @@
-import {Image,Card, CardHeader, CardBody} from "@nextui-org/react";
+import {Image, Card, CardHeader, CardBody} from "@nextui-org/react";
 import {Spacer} from "@nextui-org/react";
-
+import ContentOffer from "./ContentOffer";
 import {OfferCard} from "./OfferCard";
 
-let Offer = () =>{
-    return(
-      <><div class="Aboutmid" style={{ display: "flex", flexDirection: "column" }}>
-          <center>
-          <div style={{display:"flex",color:"#D97706", fontSize:"10px", fontWeight:"bold",justifyContent:"center"}}>
-        <h >Our Services</h>
-        </div>
-          <div style={{display:"flex",color:"black", fontSize:"30px", fontWeight:"bold",justifyContent:"center"}}>
-        <h >What we offer</h>
-        </div>
-        <div style={{display:"flex",color:"gray",fontSize:"20px",justifyContent:"center"}}>
-        <h>What we offer</h></div>
-        <div style={{display:"flex",color:"gray",fontSize:"15px",justifyContent:"center"}}>
-        <p>enjoy and protect a peaceful environment for sharing feelings</p></div>
+let Offer = () => {
+  return (
+    <>
+      <div class="Aboutmid" style={{display: "flex", flexDirection: "column"}}>
+        <center>
+          <div
+            style={{
+              display: "flex",
+              color: "#D97706",
+              fontSize: "10px",
+              fontWeight: "bold",
+              justifyContent: "center",
+            }}
+          >
+            <h>Our Services</h>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              color: "black",
+              fontSize: "30px",
+              fontWeight: "bold",
+              justifyContent: "center",
+            }}
+          >
+            <h>What we offer</h>
+          </div>
+          <div style={{display: "flex", color: "gray", fontSize: "20px", justifyContent: "center"}}>
+            <h>What we offer</h>
+          </div>
+          <div style={{display: "flex", color: "gray", fontSize: "15px", justifyContent: "center"}}>
+            <p>enjoy and protect a peaceful environment for sharing feelings</p>
+          </div>
         </center>
-      </div><div class="cards" >
-      <div className="flex" style={{flexDirection:"row", justifyContent:"center", margin:"30px"}}>
-        
-      <OfferCard />
-    </div>
-
-        </div></>
-    )
-}
+      </div>
+      <div class="cards">
+        <div
+          className="flex"
+          style={{flexDirection: "row", justifyContent: "center", margin: "30px"}}
+        >
+          {ContentOffer.map((e) => (
+            <OfferCard img={e.img} head={e.head} p1={e.p1} p2={e.p2}></OfferCard>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
 export default Offer;
--- frontend/src/pages/about/OfferCard.jsx
+++ frontend/src/pages/about/OfferCard.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import {Card, Spacer} from "@nextui-org/react";
-import ContentOffer from './ContentOffer';
 
-export const OfferCard = () => (
-  
-  <div style={{flexDirection:"row", justifyContent:"center", margin:"30px"}}><Card className="w-[200px] space-y-5 p-4" radius="2x3">
-    <div className="h-24 rounded-lg bg-default-300"></div>
-    <div className="space-y-3">
-      <div style={{ fontFeatureSettings: "inherit", fontSizeAdjust: "scale 10px", fontSize: "17px" }}>
-        <p className="font-bold">{ContentOffer.head}</p></div>
-      <div>{ContentOffer.p1}</div>
-      <div>{ContentOffer.p2}</div>
-    </div>
-  </Card><Spacer x={4} /></div> 
-
+export const OfferCard = ({img, head, p1, p2}) => (
+  <div style={{flexDirection: "row", justifyContent: "center", margin: "30px"}}>
+    <Card className="w-[200px] space-y-5 p-4" radius="2x3">
+      <div className="h-24 rounded-lg bg-default-300"></div>
+      <div className="space-y-3">
+        <div
+          style={{fontFeatureSettings: "inherit", fontSizeAdjust: "scale 10px", fontSize: "17px"}}
+        >
+          <p className="font-bold">{head}</p>
+        </div>
+        <div>{p1}</div>
+        <div>{p2}</div>
+      </div>
+    </Card>
+    <Spacer x={4} />
+  </div>
 );
