dd8ba8b52b2551d7f9d0e08d408bfb389f5f01bc
NarwhalChen <spixel.xi@gmail.com>
2024-04-23 01:52:34 -0500

    update: About page

---
+++ frontend/src/pages/about/CustomCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {Card} from "@nextui-org/react";
+
+export const CustomCard = () => (
+  
+  <Card className="w-[200px] space-y-5 p-4" radius="2x3">
+    <div className="h-24 rounded-lg bg-default-300"></div>
+    <div className="space-y-3">
+      <div style={{fontFeatureSettings:"inherit", fontSizeAdjust:"scale 10px",fontSize:"17px"}}>
+        <p className="font-bold">Wanna save a time capsule for your future?</p></div>
+      <div>- We will send this private letter to you at your targeted time at targeted location</div>
+      <div>- Open the time capsule one day in the future to revoke the current unforgettable-memories</div>
+    </div>
+  </Card>
+);
