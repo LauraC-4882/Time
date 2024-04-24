60e0e7d4cf21b54547143162f8ef62fd05d90c95
NarwhalChen <spixel.xi@gmail.com>
2024-04-24 00:24:34 -0500

    to: profile edit page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -31,6 +31,10 @@ const router = createBrowserRouter([
         path: "/profile",
         element: <ProfilePage />,
       },
+      {
+        path: "/edit",
+        element: <ProfilePage />,
+      },
     ],
   },
 ]);
--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Tabs, Tab, Card, User, CardBody, Button } from "@nextui-org/react";
 import { useNavigate } from 'react-router-dom';
 import Cad from "../utils/post";
 import People from "../utils/PeopleList.jsx";
+import EditCard from "../../components/EditCard.jsx";
+import { users } from '../../components/UserData.jsx';
 
 export default function Profile() {
-  let navigate = useNavigate();
-  function handleEditClick() {
-    // 导航到'/edit'页面
-    navigate('/edit');
-  }
+  const [isEditing, setIsEditing] = useState(false);
+  const [user, setUser] = useState({
+    name: users.name,
+    description: users.description,
+    avatar: users.avatar,
+    phone: users.phone
+  });
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleSave = (updatedUser) => {
+    setUser({
+      ...user, // Spread the existing user to maintain any existing properties
+      ...updatedUser // This will override the existing properties with those from updatedUser
+    });
+    setIsEditing(false);
+  };
+
+  const handleCancel = () => {
+    setIsEditing(false);
+  };
 
   return (
     <div style={{ display: "flex", padding: "50px 200px 50px 200px" }}> {/* This centers the Tabs vertically and horizontally on the screen */}
@@ -45,25 +65,29 @@ export default function Profile() {
           </Tab>
         </Tabs>
       </div>
-      <div className="flex column w-4/5 max-w-4xl flex-col m-4" style={{ marginRight: '80px' }}>
-        <div className="flex" style={{ marginLeft: '20%', marginTop: '10%', marginBottom: '5%' }}>
-          <div>
-            <User
-              name="Jane Doe"
-              description="Product Designer"
-              avatarProps={{
-                src: "https://i.pravatar.cc/150?u=a04258114e29026702d"
-              }}
-            />
-          </div>
+      <div className="flex column w-4/5 max-w-4xl flex-col m-4" >
+        <div className="flex" style={{ width: "100%", marginLeft: '20%', marginTop: '10%', marginBottom: '5%' }}>
 
-          <div style={{ marginLeft: '50%' }}>
-            <Button color="primary" onClick={handleEditClick}>
-              Edit
-            </Button>
-          </div>
+          {isEditing ? (
+            <EditCard
+              user={user} onSave={handleSave} onCancel={handleCancel}
+
+            />
+          ) : (
+            <div>
+              <User
+                name={users.name}
+                description={users.description}
+                avatarProps={{
+                  src: users.avatar
+                }}
+                onClick={handleEditClick}
+              />
+            </div>
+          )}
+
         </div>
-        <div style={{ marginTop: '5%', marginLeft: '20%' }}>Phone number</div>
+
       </div>
     </div>
   );
