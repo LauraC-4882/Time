eeb746d6d8c72559f7d046fd2f3524d7d71653ab
Jackson Chen <90215880+Sma1lboy@users.noreply.github.com>
2024-06-25 00:02:58 +0800

    ci: build fixing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
-import { NextUIProvider } from "@nextui-org/react";
+import {NextUIProvider} from "@nextui-org/react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import { Outlet } from "react-router-dom";
+import {Outlet} from "react-router-dom";
 function App() {
   return (
     <NextUIProvider>
@@ -12,13 +12,12 @@ function App() {
           background: "linear-gradient(rgb(246,237,189), rgba(246,237,189, 0))",
         }}
       >
-        <div style={{ height: "80vh" }}>
+        <div style={{height: "80vh"}}>
           <Outlet />
         </div>
         <Footer></Footer>
       </div>
       <div id="modal"></div>
-
     </NextUIProvider>
   );
 }
