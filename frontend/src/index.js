c4e04643815b8a36449f576340668dec8e1cd935
Jackson Chen <90215880+Sma1lboy@users.noreply.github.com>
2024-06-25 15:40:25 +0800

    to: adding firebase

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,8 @@ import AboutPage from "./pages/about/AboutPage";
 import ErrorPage from "./pages/ErrorPage";
 import Home from "./pages/home/Home";
 import LetterPage from "./pages/letter/LetterPage";
+//init firebase
+import "./firebase/index.ts";
 
 const router = createBrowserRouter([
   {
