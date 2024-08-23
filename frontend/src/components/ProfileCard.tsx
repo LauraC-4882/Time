97c6b29e6237c59377b5f460c9973c1af1b685e7
Jackson Chen <541898146chen@gmail.com>
2024-08-22 22:41:52 -0400

    to: adding user info

--- frontend/src/components/ProfileCard.tsx
+++ frontend/src/components/ProfileCard.tsx
@@ -1,57 +1,52 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Card, CardHeader, Avatar} from "@nextui-org/react";
 import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell} from "@nextui-org/react";
 import {Listbox, ListboxItem} from "@nextui-org/react";
-import {signOut} from "firebase/auth";
+import {onAuthStateChanged, signOut, User} from "firebase/auth";
 import {auth} from "../firebase/index.ts";
 
-export const ProfileCard = ({user}) => {
-  const columns = [
-    {
-      key: "name",
-      label: "NAME",
-    },
-    {
-      key: "role",
-      label: "ROLE",
-    },
-    {
-      key: "status",
-      label: "STATUS",
-    },
-  ];
-
-  const rowData = [
-    {
-      name: user.name,
-      role: user.role,
-      status: user.status,
-    },
-  ];
+export const ProfileCard = () => {
+  const [user, setUser] = useState<User>();
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(undefined);
+      }
+    });
+  }, []);
 
   return (
     <Card className="max-w-[340px]">
       <CardHeader className="justify-between">
         <div className="flex gap-5">
-          <Avatar isBordered radius="full" size="md" src={user.avatarUrl} />
+          <Avatar
+            isBordered
+            radius="full"
+            size="md"
+            src={user ? (user.photoURL ? user.photoURL : "") : undefined}
+          />
           <div className="flex flex-col gap-1 items-start justify-center">
-            <h4 className="text-small font-semibold leading-none text-default-600">{user.name}</h4>
-            <h5 className="text-small tracking-tight text-default-400">@{user.username}</h5>
+            <h4 className="text-small font-semibold leading-none text-default-600">
+              {user?.displayName}
+            </h4>
+            <h5 className="text-small tracking-tight text-default-400">@{"test"}</h5>
           </div>
         </div>
       </CardHeader>
-      <Table aria-label="Example table with dynamic content">
-        <TableHeader columns={columns}>
-          {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+      <Table aria-label="User information table">
+        <TableHeader>
+          <TableColumn>NAME</TableColumn>
+          <TableColumn>ROLE</TableColumn>
+          <TableColumn>STATUS</TableColumn>
         </TableHeader>
         <TableBody>
-          {rowData.map((data, index) => (
-            <TableRow key={index}>
-              {columns.map((column) => (
-                <TableCell key={column.key}>{data[column.key]}</TableCell>
-              ))}
-            </TableRow>
-          ))}
+          <TableRow>
+            <TableCell>{user?.displayName}</TableCell>
+            <TableCell>{user?.emailVerified}</TableCell>
+            <TableCell>{user?.emailVerified}</TableCell>
+          </TableRow>
         </TableBody>
       </Table>
       <Listbox aria-label="Dynamic Actions" onAction={(key) => alert(key)}>
